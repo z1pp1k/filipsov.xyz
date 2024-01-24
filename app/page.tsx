17e9a171725bd7750629686b12726562f6b6ea2a
z1pp1k <sovadinafilip@seznam.cz>
2024-01-24 19:00:34 +0100

    🔧: BG fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <main className="min-h-screen">
       <div className="mx-auto max-w-screen-md">
         <Navbar></Navbar>
-        <header className="mt-12 px-6">
+        <header className="mt-12 px-6 pb-20">
           <h1 className="mb-1 text-4xl font-bold text-black dark:text-white">
             Filip Sovadina
           </h1>
@@ -68,7 +68,7 @@ export default function Page() {
             </Link>
           </div>
 
-          <div className="my-10 rounded-lg bg-zinc-100 p-4 shadow-md sm:p-8 dark:bg-zinc-900">
+          <div className="mt-10 rounded-lg bg-zinc-100 p-4 shadow-md sm:p-8 dark:bg-zinc-900">
             <h1 className="mb-1 text-4xl font-bold text-black dark:text-white">
               Máte zájem o mé služby?
             </h1>
