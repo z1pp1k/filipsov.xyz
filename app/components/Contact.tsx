47bc0d3140f2246e380de7054cac7f3e8c045ab9
z1pp1k <sovadinafilip@seznam.cz>
2024-01-24 14:07:41 +0100

    🔧: Form texts

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -35,7 +35,8 @@ export const Contact = () => {
         className="mb-4 rounded-sm border border-zinc-300 p-2 text-black placeholder:text-zinc-300 focus:outline-none dark:border-zinc-600 dark:bg-zinc-900 dark:text-white dark:placeholder:text-zinc-600"
         type="text"
         name="user_name"
-        placeholder="Vaše jméno"
+        placeholder="Jméno a Příjmení"
+        required
       />
       <label className="text-lg text-black dark:text-white">E-Mail</label>
       <input
@@ -43,15 +44,17 @@ export const Contact = () => {
         type="email"
         name="user_email"
         placeholder="vaše@e-mailová.adresa"
+        required
       />
       <label className="text-lg text-black dark:text-white">Zpráva</label>
       <textarea
         className="mb-4 rounded-sm border border-zinc-300 p-2 text-black placeholder:text-zinc-300 focus:outline-none dark:border-zinc-600 dark:bg-zinc-900 dark:text-white dark:placeholder:text-zinc-600"
         name="message"
         placeholder="Vaše zpráva..."
+        required
       />
       <input
-        className="bg-zinc-400 py-4 text-white transition-all duration-300 ease-out hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-900"
+        className="bg-zinc-400 py-4 text-white transition-all duration-300 ease-out hover:cursor-pointer hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-900"
         type="submit"
         value="Odeslat"
       />
