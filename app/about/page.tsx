7648612e34d5c1007ae6fcc5ff391e751aee79e1
z1pp1k <sovadinafilip@seznam.cz>
2024-03-01 17:00:52 +0100

    🔧: Phone info change and removed useless things

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,18 +13,18 @@ export default function Page() {
       <div className="mx-auto max-w-screen-md">
         <Navbar></Navbar>
         <header className="mt-12 px-6">
-          <h1 className="animate-in text-4xl font-semibold text-black dark:text-white">
+          <h1 className="text-4xl font-semibold text-black dark:text-white">
             O mně
           </h1>
-          <p className="animate-in text-zinc-500">Pár informací o mně.</p>
-          <p className="animate-in mt-10 text-black dark:text-white">
+          <p className="text-zinc-500">Pár informací o mně.</p>
+          <p className="mt-10 text-black dark:text-white">
             Jsem studentem čtvrtého ročníku na Střední škole informačních
             technologií a většinu svého času trávím učením novým věcem.
           </p>
         </header>
         <Links></Links>
 
-        <div className="text-md flex flex-col gap-1 text-center font-light text-zinc-800 dark:text-zinc-200">
+        <div className="text-md flex flex-col gap-1 px-6 text-center font-light text-zinc-800 dark:text-zinc-200">
           <span className="font-semibold">Filip Sovadina</span>
           <span>Podroužkova 1676/61, 70800 Ostrava - Poruba</span>
           <span className="font-semibold">IČ: 21154686</span>
